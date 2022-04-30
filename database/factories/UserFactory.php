7fb3a2979abce5d59f2a54eb937cdaf670251ba8
lucasrigon <lucas@bussolasocial.com.br>
2022-04-30 14:13:05 -0300

    user factory and seed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,6 +19,9 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'uuid' => $this->faker->unique()->uuid(),
+            'document' => $this->faker->numberBetween(1000000, 9999999),
+            'document_type' => $this->faker->word(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
