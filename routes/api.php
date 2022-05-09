751e90a58bf93b7bbeda4a7436b92324ccde4e50
lucasrigon <lucas@bussolasocial.com.br>
2022-05-08 22:01:30 -0300

    route prefix add

--- routes/api.php
+++ routes/api.php
@@ -15,30 +15,31 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::prefix('type')->group(function () {
+    Route::get('payments-type/', [PaymentTypeController::class, 'index'])
+        ->name('payment.type.index');
+    Route::get('payments-type/search', [PaymentTypeController::class, 'search'])
+        ->name('payment.type.search');
+    Route::post('payments-type/store', [PaymentTypeController::class, 'store'])
+        ->name('payment.type.store');
+    Route::put('payments-type/{id}/update', [PaymentTypeController::class, 'update'])
+        ->name('payment.type.update');
+    Route::delete('payments-type/{id}/delete', [PaymentTypeController::class, 'delete'])
+        ->name('payment.type.delete');
+    Route::put('payments-type/{id}/restore', [PaymentTypeController::class, 'restore'])
+        ->name('payment.type.restore');
 
-Route::get('type/payments-type/', [PaymentTypeController::class, 'index'])
-    ->name('payment.type.index');
-Route::get('type/payments-type/search', [PaymentTypeController::class, 'search'])
-    ->name('payment.type.search');
-Route::post('type/payments-type/store', [PaymentTypeController::class, 'store'])
-    ->name('payment.type.store');
-Route::put('type/payments-type/{id}/update', [PaymentTypeController::class, 'update'])
-    ->name('payment.type.update');
-Route::delete('type/payments-type/{id}/delete', [PaymentTypeController::class, 'delete'])
-    ->name('payment.type.delete');
-Route::put('type/payments-type/{id}/restore', [PaymentTypeController::class, 'restore'])
-    ->name('payment.type.restore');
 
-
-Route::get('type/payments-value/', [PaymentValueController::class, 'index'])
-    ->name('payment.value.index');
-Route::get('type/payments-value/search', [PaymentValueController::class, 'search'])
-    ->name('payment.value.search');
-Route::post('type/payments-value/store', [PaymentValueController::class, 'store'])
-    ->name('payment.value.store');
-Route::put('type/payments-value/{id}/update', [PaymentValueController::class, 'update'])
-    ->name('payment.value.update');
-Route::delete('type/payments-value/{id}/delete', [PaymentValueController::class, 'delete'])
-    ->name('payment.value.delete');
-Route::put('type/payments-value/{id}/restore', [PaymentValueController::class, 'restore'])
-    ->name('payment.value.restore');
+    Route::get('payments-value/', [PaymentValueController::class, 'index'])
+        ->name('payment.value.index');
+    Route::get('payments-value/search', [PaymentValueController::class, 'search'])
+        ->name('payment.value.search');
+    Route::post('payments-value/store', [PaymentValueController::class, 'store'])
+        ->name('payment.value.store');
+    Route::put('payments-value/{id}/update', [PaymentValueController::class, 'update'])
+        ->name('payment.value.update');
+    Route::delete('payments-value/{id}/delete', [PaymentValueController::class, 'delete'])
+        ->name('payment.value.delete');
+    Route::put('payments-value/{id}/restore', [PaymentValueController::class, 'restore'])
+        ->name('payment.value.restore');
+});
